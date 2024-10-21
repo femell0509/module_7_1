0544465244ad31d59bf6bf084bb72ac609b510c3
femell0509 <kirillvah.com@gmail.com>
2024-10-21 17:43:43 +0300

    Add files via upload

---
+++ modul_7_1.py
@@ -0,0 +1,38 @@
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop():
+    __fale_name = 'products.txt'
+    def get_products(self):
+        file = open(self.__fale_name, 'r')
+        str_products = file.read()
+        file.close()
+        return str_products
+
+    def add(self, *products):
+        control_prod = self.get_products()
+        for i in products:
+            if i.name in control_prod:
+                print(f'Продукт {i.name} уже есть в магазине')
+            else:
+                file = open(self.__fale_name, 'a')
+                file.write(f'{i}\n')
+                file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+#
+s1.add(p1, p2, p3)
+#
+print(s1.get_products())
